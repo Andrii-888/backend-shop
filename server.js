94c4ba81c8794276f7d6a421f9f45d7ccd04e003
Andrii-888 <122465780+Andrii-888@users.noreply.github.com>
2024-09-27 15:43:28 +0200

    session delete

--- server.js
+++ server.js
@@ -32,72 +32,72 @@ app.use(routes);
 // seedData();
 
 // Функция для генерации уникального идентификатора сессии
-function generateSessionId() {
-  return crypto.randomBytes(16).toString("hex");
-}
+// function generateSessionId() {
+//   return crypto.randomBytes(16).toString("hex");
+// }
 
 // Роут для создания и получения сессии
-app.post("/api/session", async (req, res) => {
-  console.log(15);
-  try {
-    // Извлечение идентификатора сессии из куки
-    const sessionId = req.cookies["sessionId"];
-    console.log(sessionId, 999);
-    if (!sessionId) {
-      // Если идентификатор сессии не найден, создаем новый
-      const newSessionId = generateSessionId();
-      const newSession = db.Session({
-        sessionId: newSessionId,
-        userId: generateUserId(), // Вы можете заменить на реальный идентификатор
-        cart: [],
-        favorite: [],
-      });
-      // console.log(newSession, 23456);
-      await newSession.save();
-      console.log(newSessionId, 909);
-      // Устанавливаем куку с идентификатором новой сессии
-      res.cookie("sessionId", newSessionId, {
-        maxAge: 60000 * 60 * 24 * 30, // 30 дней
-        httpOnly: true,
-        // sameSite: "Lax", // Защита от CSRF атак
-        sameSite: "None", // Измените на "None"
-        secure: false, // Убедитесь, что secure: false на localhost
-      });
-      console.log("Headers Sent:", res.headersSent);
-      console.log("Request Cookies:", req.cookies["sessionId"]); // Посмотрите, какие куки приходят
-      console.log("Request Origin:", req.headers.origin); // Убедитесь, что Origin совпадает с ожидаемым
-      // Ваш код дальше...
+// app.post("/api/session", async (req, res) => {
+//   console.log(15);
+//   try {
+//     // Извлечение идентификатора сессии из куки
+//     const sessionId = req.cookies["sessionId"];
+//     console.log(sessionId, 999);
+//     if (!sessionId) {
+//       // Если идентификатор сессии не найден, создаем новый
+//       const newSessionId = generateSessionId();
+//       const newSession = db.Session({
+//         sessionId: newSessionId,
+//         userId: generateUserId(), // Вы можете заменить на реальный идентификатор
+//         cart: [],
+//         favorite: [],
+//       });
+//       // console.log(newSession, 23456);
+//       await newSession.save();
+//       console.log(newSessionId, 909);
+//       // Устанавливаем куку с идентификатором новой сессии
+//       res.cookie("sessionId", newSessionId, {
+//         maxAge: 60000 * 60 * 24 * 30, // 30 дней
+//         httpOnly: true,
+//         // sameSite: "Lax", // Защита от CSRF атак
+//         sameSite: "None", // Измените на "None"
+//         secure: false, // Убедитесь, что secure: false на localhost
+//       });
+//       console.log("Headers Sent:", res.headersSent);
+//       console.log("Request Cookies:", req.cookies["sessionId"]); // Посмотрите, какие куки приходят
+//       console.log("Request Origin:", req.headers.origin); // Убедитесь, что Origin совпадает с ожидаемым
+//       // Ваш код дальше...
 
-      res.json({
-        sessionId: newSessionId,
-        userId: newSession.userId,
-        cart: newSession.cart,
-        favorite: newSession.favorite,
-      });
-    } else {
-      // Если идентификатор сессии существует, извлекаем сессию из базы данных
-      const session = await db.Session.findOne({ sessionId });
+//       res.json({
+//         sessionId: newSessionId,
+//         userId: newSession.userId,
+//         cart: newSession.cart,
+//         favorite: newSession.favorite,
+//       });
+//     } else {
+//       // Если идентификатор сессии существует, извлекаем сессию из базы данных
+//       const session = await db.Session.findOne({ sessionId });
 
-      if (!session) {
-        return res.status(404).json({ error: "Session not found" });
-      }
+//       if (!session) {
+//         return res.status(404).json({ error: "Session not found" });
+//       }
 
-      res.json({
-        sessionId: session.sessionId,
-        userId: session.userId,
-        cart: session.cart,
-        favorite: session.favorite,
-      });
-    }
-  } catch (error) {
-    console.error("Error handling session:", error);
-    res.status(500).json({ error: "Failed to handle session" });
-  }
-});
+//       res.json({
+//         sessionId: session.sessionId,
+//         userId: session.userId,
+//         cart: session.cart,
+//         favorite: session.favorite,
+//       });
+//     }
+//   } catch (error) {
+//     console.error("Error handling session:", error);
+//     res.status(500).json({ error: "Failed to handle session" });
+//   }
+// });
 
-function generateUserId() {
-  return Math.random().toString(36).substring(2, 15);
-}
+// function generateUserId() {
+//   return Math.random().toString(36).substring(2, 15);
+// }
 
 // app.listen(PORT, function () {
 //   console.log(`app listen on port ${PORT}`);
